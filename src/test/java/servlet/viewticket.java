db53bcdb88267683bd4be8cbae110b7d6c51cf0a
sagardadheech <40050217+sagardadheech@users.noreply.github.com>
2021-02-08 15:07:21 -0500

    Update viewticket.java
    Added GCP IP

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://3.19.232.247:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://104.198.17.130:5432/postgres";
 			String username="postgres";
 			String password="password";
 						
